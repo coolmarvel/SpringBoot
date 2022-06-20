ead537a12fb08ebb99ac887b2b3dd601d9682138
이성현 <marvel97@naver.com>
2022-06-20 13:14:41 +0900

    employee management system(CRUD)

---
+++ employeemanagementsystem/src/main/java/com/exam/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.exam.employeemanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.exam.employeemanagementsystem.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
